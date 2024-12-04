cbbdd6ef086c9fdcf4f77a2ef4b6062ef452c5f9
Pepijn <437675@student.fontys.nl>
2024-12-04 14:03:02 +0100

    added endpoint to get BearerToken

--- SpotifyMusicQuizApi/Controllers/AuthorizationController.cs
+++ SpotifyMusicQuizApi/Controllers/AuthorizationController.cs
@@ -1,27 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
-
 namespace SpotifyMusicQuizApi.Controllers
 {
     [ApiController]
     [Route("[controller]")]
     public class AuthorizationController : ControllerBase
     {
-        private string token;
-        private DateTime expireTime;
-
         private readonly ILogger<AuthorizationController> _logger;
 
         public AuthorizationController(ILogger<AuthorizationController> logger)
         {
             _logger = logger;
-            token = "";
         }
 
+        
         [HttpGet(Name = "GetToken")]
-        public string Get()
+        public string GetToken()
         {
-
-            return token;
+            return Token.GetToken();
         }
     }
 }
---
+++ SpotifyMusicQuizApi/Token.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RestSharp;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text.Json;
+
+namespace SpotifyMusicQuizApi
+{
+    public static class Token
+    {
+        private static string token = "";
+        private static DateTime expireTime = DateTime.UtcNow;
+
+        private static string clientId;
+        private static string clientSecret;
+
+        static Token()
+        {
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            clientId = config["CLIENT_ID"];
+            clientSecret = config["CLIENT_SECRET"];
+
+            if (clientId == null) clientId = "";
+            if (clientSecret == null) clientSecret = "";
+        }
+
+        public static string GetToken()
+        {
+            if (expireTime < DateTime.UtcNow) {
+                var client  = new RestClient("https://accounts.spotify.com");
+                var request = new RestRequest("api/token");
+                request.AddParameter("grant_type", "client_credentials");
+                request.AddParameter("client_id", clientId);
+                request.AddParameter("client_secret", clientSecret);
+                request.AddHeader("Content-Type", "application/x-www-form-urlencoded");
+                var response = client.Post(request);
+                var content = response.Content;
+                if (content != null)
+                {
+
+                    token = JsonDocument.Parse(content).RootElement.GetProperty("access_token").ToString();
+                    string time = JsonDocument.Parse(content).RootElement.GetProperty("expires_in").ToString(); ;
+                    expireTime = DateTime.UtcNow.AddSeconds(Convert.ToInt32(time));
+                }
+
+                return token;
+            }
+            return token;
+        }
+        
+    }
+}
